7e9049922c7f1a0e6d17a21b6fda59788b907d18
Mikayla <user@users-MBP.attlocal.net>
2023-05-10 16:51:05 -0400

    created eventlistener for submit form

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,5 @@
+var apiKey = "901e7cc22bdd833a977d9e48c937d9b4";
+
 var searchForm = document.getElementById('searchForm');
 var inputCity = document.getElementById('cityInput');
 var cityName = document.getElementById('cityName');
@@ -8,3 +10,14 @@ var humidity = document.getElementById('humidity');
 var windSpeed = document.getElementById('windSpeed');
 var forecastCards = document.getElementById('forecastCards');
 var historyList = document.getElementById('historyList');
+
+
+
+searchForm.addEventListener('submit', function(event){
+    event.preventDefault();
+    var city = inputCity.value();
+    if (city) {
+        getWeather(city);
+        inputCity.value = '';
+    }
+});
