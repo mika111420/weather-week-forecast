4e48e46fe85c9a3ece531463f77ccb3b86646cee
Mikayla <user@users-MBP.attlocal.net>
2023-05-11 18:56:51 -0400

    added fetch function to JS

--- assets/script.js
+++ assets/script.js
@@ -2,16 +2,28 @@ var apiKey = "901e7cc22bdd833a977d9e48c937d9b4";
 
 var searchForm = document.getElementById('searchForm');
 var inputCity = document.getElementById('cityInput');
+
 var cityName = document.getElementById('cityName');
 var date = document.getElementById('date');
 var weatherIcon = document.getElementById('weatherIcon');
 var temperature = document.getElementById('temperature');
 var humidity = document.getElementById('humidity');
 var windSpeed = document.getElementById('windSpeed');
+
 var forecastCards = document.getElementById('forecastCards');
 var historyList = document.getElementById('historyList');
 
+function getWeather(city) {
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
 
+fetch(queryURL)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function (data) {
+        displayCurrentWeather(data);
+      })
+};
 
 searchForm.addEventListener('submit', function(event){
     event.preventDefault();
@@ -35,14 +47,21 @@ function displayForecast(dayForecast) {
         temperature.textContent = `Temperature: ${day.temperature}`;
         card.appendChild(temperature);
 
-        const windSpeed = document.createElement('div');
+        var windSpeed = document.createElement('div');
         windSpeed.textContent = `Wind Speed: ${day.windSpeed}`;
         card.appendChild(windSpeed);
 
-        const humidity = document.createElement('div');
+        var humidity = document.createElement('div');
         humidity.textContent = `Humidity: ${day.humidity}`;
         card.appendChild(humidity);
 
         forecastCards.appendChild(card);
     }
 }
+
+function addSearchHistory (city) {
+    var historyCity = document.createElement('li');
+    historyCity.textContent = city;
+    historyList.appendChild(historyCity);
+}
+
